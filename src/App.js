6d447cb9a2bda4bfdac2c6d0f155ca7cb8757e7e
Casey Williams <caseywilliams@users.noreply.github.com>
2020-09-03 13:37:05 -0700

    Demo tooltip behavior

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Button, TooltipHoverArea } from '@puppet/react-components'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: 2500, overflow: 'scroll' }}>
+      <TooltipHoverArea tooltip="I'm a happy tooltip!" anchor="bottom">
+        <Button>I'm a happy button!</Button>
+      </TooltipHoverArea>
     </div>
   );
 }
